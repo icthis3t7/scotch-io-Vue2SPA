ce7a17eae33c0f474a7e5f4ddb0ca6a59525acbc
icthis3t7 <colby@colbys-macbook.attlocal.net>
2017-03-27 19:09:02 -0500

    implemented router

--- src/main.js
+++ src/main.js
@@ -1,15 +1,22 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import VueRouter from 'vue-router';
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
 
-/* eslint-disable no-new */
+const routes = [];
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+
+});
+
+
 new Vue({
   el: '#app',
-  router,
   template: '<App/>',
-  components: { App }
-})
+  components: { App },
+  router
+
+}).$mount('#app');
