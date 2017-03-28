838ac64f491738775107736021c823d2f167b1ee
icthis3t7 <colby@colbys-macbook.attlocal.net>
2017-03-27 19:26:32 -0500

    wow that tutorial was short :/

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <router-link v-bind:to="'/'">Home</router-link>
+    <router-link v-bind:to="'/about'">About</router-link>
     <router-view></router-view>
 
   </div>
